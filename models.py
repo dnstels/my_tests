4e5a64b75ce9928976073eebc5e92aade18b4ae1
dnstels <35730176+dnstels@users.noreply.github.com>
2024-10-11 16:34:03 +0400

    Update models.py

--- models.py
+++ models.py
@@ -70,6 +70,7 @@ class Flowline:
     def to_json(self):
         return json.dumps(self.__dict__,cls=tools.JsonClassEncoder,indent=4)
 
+@dataclass
 class Names():
     Flowline:[]
     '''Трубопроводы'''
@@ -82,15 +83,20 @@ class Names():
     Choke:[]
     '''Штуцеры'''
 
-    def __init__(self,**data) -> None:
-        self.Flowline=data.get('Flowline',[])
-        self.Source=data.get('Source',[])
-        self.Sink=data.get('Sink',[])
-        self.Junction=data.get('Junction',[])
-        self.Choke=data.get('Choke',[])
+    # def __init__(self,**data) -> None:
+    #     self.Flowline=data.get('Flowline',[])
+    #     self.Source=data.get('Source',[])
+    #     self.Sink=data.get('Sink',[])
+    #     self.Junction=data.get('Junction',[])
+    #     self.Choke=data.get('Choke',[])
     @staticmethod
     def get_types():
-        return list(Names.__annotations__)
+        return tuple(Names.__annotations__)
+    @staticmethod
+    def get_valid_node_types():
+        return ('Source','Sink','Junction')
+    def get_valid_nodes(self):
+        return self.Source+self.Sink+self.Junction
 
 @dataclass
 class Source:
@@ -108,14 +114,18 @@ class Sink:
     GasFlowRate:float
     '''Расход газа'''
 
-@dataclass
+@dataclass(frozen=True)
 class Branch():
-    __slots__ = ('Source','Destination')
     Source:str
     Destination:str
-    def __init__(self,**data):
-        self.Source=data.get('Source',None)
-        self.Destination=data.get('Destination',None)
+    def validate(self):
+        return self.Source != None and self.Destination != None
+    @staticmethod
+    def get_orients():
+        return Branch.__annotations__
+    @staticmethod
+    def get_revers_orient(orient:str):        
+        return tools.get_first_or_none([s for s in Branch.get_orients() if s != orient])
 
 class ProjectDataModel():
     # __slots__ = ('pipesim_file','project_name')
@@ -123,29 +133,58 @@ class ProjectDataModel():
         # self.__prefix=f'_{__class__.__name__}__'
         self.pipesim_file=''
         self.project_name=''
-        self.__Network=[Branch(**b) for b in data.get('Network',[])]
-        self.__Names=data.get('Names',{})
+        self.__Network=[Branch(**tools.norm_dict(Branch,b)) for b in data.get('Network',[])]
+        self.__Names=Names(**tools.norm_dict(Names,data.get('Names',{})))
         self.__FlowlineData={k:Flowline(**v) for k,v in data.get('FlowlineData',{}).items()}
         self.__InData=data.get('InData',{})
         self.__OutData=data.get('OutData',{})
     
     @property
-    def Names(self):return self.__Names
+    def Names(self):
+        return self.__Names
     
     @property
-    def Network(self):return self.__Network
+    def Network(self):
+        return self.__Network
     @Network.setter  
     def Network(self,network:Network): self.__Network=network
     
     @property
-    def FlowlineData(self):return self.__FlowlineData
-    @Network.setter 
+    def FlowlineData(self):
+        return self.__FlowlineData
+    @FlowlineData.setter 
     def FlowlineData(self,flowlines:dict): self.__FlowlineData=flowlines#{k:Flowline(**v) for k,v in flowlines.items()}
 
     @property
     def InData(self):return self.__InData
     @property
     def OutData(self):return self.__OutData
+
+    def _get_valid_node(self,branch_name:str,orientation:str):
+        find_name=branch_name
+        valid_nodes_name=self.__Names.get_valid_nodes()
+        while True:
+            # print(f'orientation {orientation}, {find_name}:')
+            # print([tools.get_atter(node,orientation) for node in self.__Network if tools.get_atter(node,Branch.get_revers_orientation(orientation)) == find_name])
+            find_node_name=next(
+                iter([tools.get_atter(node,orientation) for node in self.__Network 
+                      if tools.get_atter(node,Branch.get_revers_orient(orientation)) == find_name])
+                ,None)
+            # print(find_node_name)
+            if find_node_name == None or find_node_name in valid_nodes_name:
+                break
+            find_name=find_node_name
+        return find_node_name
+    
+    def get_trace(self,branch_name,useBranch:bool=False):
+        '''
+        Определяет начало и конец трубопровода
+        '''
+        out={slot:self._get_valid_node(branch_name,slot) for slot in Branch.get_orients()}
+        return { branch_name:Branch(**out) if useBranch else out }
+
+
+
 
     def get_attr(self,attr_name: str):
         return self.__getattribute__(attr_name) if hasattr(self,attr_name) else None
