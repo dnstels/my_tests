dcb53760490d231add8a5ac0e7f8184c9756e01c
Stels Stels <d_stels@mail.ru>
2025-01-02 17:34:34 +0400

    get_result_of_number в одну строчку

--- operator_if/fun_if.py
+++ operator_if/fun_if.py
@@ -1,2 +1,1 @@
-def get_result_of_number(num):
-    return 'Положительное' if num>0 else 'Отрицательное' if num<0 else 'Ноль'
+def get_result_of_number(num) : return 'Положительное' if num>0 else 'Отрицательное' if num<0 else 'Ноль'
