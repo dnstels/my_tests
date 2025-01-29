6d15d2da29cba2658db2cc7f59b5528311b613b7
dnstels <35730176+dnstels@users.noreply.github.com>
2025-01-29 17:56:56 +0400

    Update test_rest.py

--- dash/tests/test_rest.py
+++ dash/tests/test_rest.py
@@ -1,5 +1,8 @@
+import sys
 import pytest
-from get_rest import get_LabourCost, get_fake, get_from
+sys.path.append('.')
+
+from get_rest import create_request_department_for_LabourCost, get_fake_graph_data, get_from
 
 
 def test_get_from():
@@ -7,19 +10,17 @@ def test_get_from():
     assert res == None
     assert err != None
 
-def test_get_LabourCost():
-    err,res=get_LabourCost(None)
-    assert res == None
-    assert err != None
-
 def test_print_fakedata():
-    txt=get_fake()
+    txt=get_fake_graph_data()
     # print(txt.to_json())
     res=f"""{{['weekNumberOfThePlanEndDate':]}}"""
     print(txt)
 
 @pytest.mark.parametrize("test_input,expected",[
-    (None,None),("Строка","%D0%A1%D1%82%D1%80%D0%BE%D0%BA%D0%B0"),
-    (["str1","str2"],"str1%2Cstr2"),(["str1"],"str1"),([],"")])
-def test_create_request_department(test_input,expected):
-    assert create_request_department(test_input) == expected
+    (None,''),
+    ("Строка","%D0%A1%D1%82%D1%80%D0%BE%D0%BA%D0%B0"),
+    (["str1","str2"],"str1%2Cstr2"),
+    (["str1"],"str1"),([],"")
+    ])
+def test_create_request_department_for_LabourCost(test_input,expected):
+    assert create_request_department_for_LabourCost(test_input) == expected
